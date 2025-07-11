793fbe7c4476ff0d0636ff364c24edb7af7bdd20
Mayur Tekale <mayurtekale7@gmail.com>
2025-07-11 10:01:29 +0530

    pages created

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="movie-page flex flex-col items-center justify-center h-screen">
+        <h1>About Page</h1>
+        <p>Details about  will be displayed here.</p>
+        {/* You can add more components or details about the movie here */}
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/Movie.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Movie = () => {
+  return (
+    <div className='movie-page flex flex-col items-center justify-center h-screen'>
+      <h1>Movie Page</h1>
+      <p>Details about the movie will be displayed here.</p>
+      {/* You can add more components or details about the movie here */}
+    </div>
+  )
+}
+
+export default Movie
