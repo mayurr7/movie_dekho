c8710821ae7bb0e74269381dae095aba4192d072
Mayur Tekale <mayurtekale7@gmail.com>
2025-08-21 08:42:51 +0530

    useNavigation for loading state

--- src/component/layout/AppLayout.jsx
+++ src/component/layout/AppLayout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
+import LoadingCom from './LoadingCom'
 
 const AppLayout = () => {
+  const navigation = useNavigation();
+
+  if (navigation.state === 'loading') return <LoadingCom />;
   return (
     <>
     <Header />
